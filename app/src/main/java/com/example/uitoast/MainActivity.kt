8e82fcbd1a27d74c38a1dbed5c08f139ba846367
Deependra Kumawat <kumawatdeependra43@gmail.com>
2023-04-25 22:22:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/uitoast/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.uitoast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import com.example.uitoast.databinding.ActivityMainBinding
+import www.sanju.motiontoast.MotionToast
+import www.sanju.motiontoast.MotionToastStyle
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btn.setOnClickListener {
+            MotionToast.createToast(this,
+                "Hurray success 😍",
+                "Upload Completed successfully!",
+                MotionToastStyle.SUCCESS,
+                MotionToast.GRAVITY_BOTTOM,
+                MotionToast.LONG_DURATION,
+                ResourcesCompat.getFont(this, www.sanju.motiontoast.R.font.helvetica_regular))
+        }
+
+    }
+}
